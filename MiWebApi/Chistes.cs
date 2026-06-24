7623b7dba82f128a73a30adf18c4a574c1e5c373
“pablobelmonteok-fallo” <pablobelmontecrack@gmail.com>
2026-06-24 11:17:59 -0300

    ejercicio 3 cargado

---
+++ MiWebApi/Chistes.cs
@@ -0,0 +1,11 @@
+public class Chistes
+{
+    public List<Jokes>? jokes{get;set;}
+}
+public class Jokes
+{
+    public string? setup {get;set;}
+    public string? delivery{get;set;}
+     public string? joke{get;set;}
+
+}
---
+++ MiWebApi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Dynamic;
+using System.Net.Sockets;
+using System.Text.Json;
+HttpClient client = new HttpClient(); // creo una instancia de HttpClient 
+HttpResponseMessage response = await client.GetAsync("https://v2.jokeapi.dev/joke/Programming?lang=es&amount=6");// en url especificamos la dirección web donde consultamos el servicio
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync(); // lee el archivo de la direccion que nosotros colocamos en url
+Chistes? chistes = JsonSerializer.Deserialize<Chistes>(responseBody); // deserializa el archivo para nosotros poder trabajar
+
+
+    foreach(var ch in chistes.jokes) // recorro la lista de jokes que esta dentro del objeto chistes
+    {
+        Console.WriteLine("------------- Nuevo Chiste -------------");
+         if (ch.setup == null && ch.delivery == null)  // si las variables vienen vacias entonces escribo otro nombre de variable 
+         {
+            Console.WriteLine("comienzo:"+ch.joke);
+    }
+    else //si las variables vienen cargadas con informacion, escribimos con nombre de las variables que corresponde 
+    {
+        Console.WriteLine("comienzo : "+ch.setup); 
+         Console.WriteLine(":"+ch.delivery);
+    }
+         
+    }
+    
+
+
+
